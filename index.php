76d81170851205e296cf9df27fa1312eb689872d
Andrew Brown <andrew@casabrown.com>
2012-09-29 11:04:03 -0400

    Add error handling to index

--- index.php
+++ index.php
@@ -8,29 +8,43 @@ require 'server/classes/Social.php';
 
 // start capture
 ob_start();
-
 ?>
 <h2>Browse</h2>
 <?php
-
 // get library
 $library = new Library();
 $_GET['width'] = 200;
 $magazines = $library->GET();
 foreach ($magazines->items as $id => $magazine):
-    $page = new Page($id, 1);
-    $page->GET();
-    $url = WebUrl::getSiteUrl() . 'service.php' . DS . 'magazine' . DS . $id;
-    
-    ?>
-    <a href="<?php echo $url ?>">
+    try {
+        $page = new Page($id, 1);
+        $page->GET();
+        $url = WebUrl::getSiteUrl() . 'service.php' . DS . 'magazine' . DS . $id;
+        ?>
+        <a href="<?php echo $url ?>">
+            <div class="magazine-thumbnail">
+                <img src="<?php echo $page->image; ?>" title="<?php echo $magazine->title; ?>" alt="<?php echo $magazine->title; ?>"/>
+                <h4><?php echo $magazine->title; ?></h4>
+            </div>
+        </a>
+        <?php
+    } catch (Error $e) {
+        ?>
         <div class="magazine-thumbnail">
-            <img src="<?php echo $page->image; ?>" title="<?php echo $magazine->title; ?>" alt="<?php echo $magazine->title; ?>"/>
-            <h4><?php echo $magazine->title; ?></h4>
+            <h4><?php echo $e->http_code; ?> <?php echo $e->http_message; ?></h4>
+            <hr class="title"/>
+            <p><span class="property">Message</span>: <?php echo $e->message; ?></p>
+            <p><span class="property">Thrown at</span>: <?php echo $e->file; ?>(<?php echo $e->line; ?>)</p>
+            <pre>Thrown at <?php echo $e->file; ?>(<?php echo $e->line; ?>)
+                <?php
+                foreach ($e->trace as $line) {
+                    echo $line . "\n";
+                }
+                ?>
+            </pre>
         </div>
-    </a>
-<?php
-
+        <?php
+    }
 endforeach;
 
 // end capture
@@ -38,6 +52,6 @@ $content = ob_get_clean();
 
 // templating
 $template = new WebTemplate('server/ui/site-template.php', WebTemplate::PHP_FILE);
-$template->replace('title', 'Browse Purple Cow Magazines');
+$template->replace('title', 'StatMags');
 $template->replace('content', $content);
 $template->display();
