ed9ac1e3571b06af229db653c2239f2ee8641f5c
Andrew Brown <andrew@casabrown.com>
2012-11-01 15:03:26 -0400

    Fix format

--- service.php
+++ service.php
@@ -1,21 +1,1 @@
-<?php
-
-require 'server/pocket-knife/start.php';
-add_include_path('server/classes');
-
-// disable time limit; if not, under load, image processing may fail
-set_time_limit(0);
- 
-// create configuration
-$configuration = new Settings(get_base_dir().'/../configuration.json');
-
-// create service
-$service = new Service($configuration);
-
-// convert application/xml to text/html; some browsers--especially mobile browsers--default to XML
-if (WebHttp::getAccept() == 'application/xml') {
-    $service->accept = 'text/html';
-}
-
-// execute
-$service->execute();
+<?phprequire 'server/pocket-knife/start.php';add_include_path('server/classes');// disable time limit; if not, under load, image processing may failset_time_limit(0);// create configuration$configuration = new Settings(get_base_dir() . '/../configuration.json');// create service$service = new Service($configuration);// convert application/xml to text/html; some browsers--especially mobile browsers--default to XMLif (WebHttp::getAccept() == 'application/xml') {    $service->accept = 'text/html';}// execute$service->execute();
